1ad74a3b9e4d9e4f8677b73935a7187c6d06f981
Vadim Korolyov <vadim.korolev.2017@mail.ru>
2023-04-27 20:15:07 +0300

    add member controller

---
+++ MyToDo.WebApi/Controllers/MembersController.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MyToDo.Application.CQRS.Members.Commands.LoginCommand;
+using MyToDo.Application.CQRS.Members.Commands.RegisterCommand;
+
+namespace MyToDo.WebApi.Controllers;
+
+public sealed class MembersController : BaseController
+{
+    public MembersController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginCommand command)
+    {
+        var result = await Mediator.Send(command);
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok();
+    }
+
+    [HttpPost("registration")]
+    public async Task<IActionResult> RegisterAsync([FromBody] RegisterCommand command)
+    {
+        var result = await Mediator.Send(command);
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok();
+    }
+}
